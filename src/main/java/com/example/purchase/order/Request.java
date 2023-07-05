a23147e923c9fa6268553b30ddcdcde4bfb8e2c3
Kavishka Jayasinghe <kavishkanethanjana@gmail.com>
2023-07-04 22:29:31 -0700

    updated model class with column mapping

--- src/main/java/com/example/purchase/order/Request.java
+++ src/main/java/com/example/purchase/order/Request.java
@@ -1,4 +1,41 @@
 package com.example.purchase.order;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Request {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column
+    private int quantity;
+
+    @Column
+    private double unitPrice;
+
+    @Column
+    private String itemColor;
+
+    @Column
+    private String po;
+
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate date;
 }
