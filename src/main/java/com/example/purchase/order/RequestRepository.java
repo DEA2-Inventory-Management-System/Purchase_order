18a2bd3c6c28551943144a1a726da9565762cf5b
Kavishka Jayasinghe <kavishkanethanjana@gmail.com>
2023-07-04 22:45:58 -0700

    included application property file

--- src/main/java/com/example/purchase/order/RequestRepository.java
+++ src/main/java/com/example/purchase/order/RequestRepository.java
@@ -1,4 +1,9 @@
 package com.example.purchase.order;
 
-public interface RequestRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface RequestRepository extends JpaRepository<Request,Integer>, JpaSpecificationExecutor<Request> {
+
+
 }
